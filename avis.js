1519da2029e09e3c5719e673d4e0ad632e11012e
misterO <mistero4321@gmail.com>
2025-04-28 15:44:55 +0200

    lessons finished

---
+++ avis.js
@@ -0,0 +1,123 @@
+export function ajoutListenersAvis() {
+
+    const piecesElements = document.querySelectorAll(".fiches article button");
+
+    for (let i = 0; i < piecesElements.length; i++) {
+
+        piecesElements[i].addEventListener("click", async function (event) {
+
+            const id = event.target.dataset.id;
+            const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+            const avis = await reponse.json();
+            window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
+            const pieceElement = event.target.parentElement;
+            afficherAvis(pieceElement, avis)
+        });
+
+    }
+}
+
+export function afficherAvis(pieceElement, avis) {
+    const avisElement = document.createElement("p");
+    for (let i = 0; i < avis.length; i++) {
+        avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+    }
+    pieceElement.appendChild(avisElement);
+}
+
+export function ajoutListenerEnvoyerAvis() {
+    const formulaireAvis = document.querySelector(".formulaire-avis");
+    formulaireAvis.addEventListener("submit", function (event) {
+        event.preventDefault();
+        // Création de l’objet du nouvel avis.
+        const avis = {
+            pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+            utilisateur: event.target.querySelector("[name=utilisateur]").value,
+            commentaire: event.target.querySelector("[name=commentaire]").value,
+            nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
+        };
+        // Création de la charge utile au format JSON
+        const chargeUtile = JSON.stringify(avis);
+        // Appel de la fonction fetch avec toutes les informations nécessaires
+        fetch("http://localhost:8081/avis", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile
+        });
+    });
+
+}
+
+export async function afficherGraphiqueAvis() {
+    // Calcul du nombre total de commentaires par quantité d'étoiles attribuées
+    const avis = await fetch("http://localhost:8081/avis").then(avis => avis.json());
+    const nb_commentaires = [0, 0, 0, 0, 0];
+
+    for (let commentaire of avis) {
+        nb_commentaires[commentaire.nbEtoiles - 1]++;
+    }
+    // Légende qui s'affichera sur la gauche à côté de la barre horizontale
+    const labels = ["5", "4", "3", "2", "1"];
+    // Données et personnalisation du graphique
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: "Étoiles attribuées",
+            data: nb_commentaires.reverse(),
+            backgroundColor: "rgba(255, 230, 0, 1)", // couleur jaune
+        }],
+    };
+    // Objet de configuration final
+    const config = {
+        type: "bar",
+        data: data,
+        options: {
+            indexAxis: "y",
+        },
+    };
+    // Rendu du graphique dans l'élément canvas
+    const graphiqueAvis = new Chart(
+        document.querySelector("#graphique-avis"),
+        config,
+    );
+}
+
+export async function afficherGraphiqueCommentaires(pieces) {
+    let nb_commentaires_disponibles = 0;
+    let nb_commentaires_non_disponibles = 0;
+
+    for (let i=0; i< pieces.length;i++) {
+        const id = pieces[i].id
+        const avis = await fetch(`http://localhost:8081/pieces/${id}/avis`).then(avis => avis.json())
+
+        if ( pieces[i].disponibilite ){
+            for (let av of avis) {
+                nb_commentaires_disponibles += av.commentaire ? 1 : 0
+            }
+        }else{
+            for (let av of avis) {
+                nb_commentaires_non_disponibles += av.commentaire ? 1 : 0   
+            }
+        }
+    }
+    
+    const data = {
+        labels: ["Pièces disponibles","Pièces non disponibles"],
+        datasets: [{
+            label: "Commentaires",
+            data: [nb_commentaires_disponibles,nb_commentaires_non_disponibles],
+            backgroundColor: "rgb(0, 94, 255)",
+        }]
+    }
+    const config = {
+        type: "bar",
+        data: data,
+        option: {
+            indexAxis: "y",
+        }
+    }
+    const graphiqueCommentaire = new Chart(
+        document.querySelector("#graphique-commentaire"),
+        config
+    )
+}
